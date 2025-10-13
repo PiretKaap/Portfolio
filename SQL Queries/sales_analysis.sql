621f4f6b4f8cb6bf3ef28fc38fa59aac80a0621e
PiretKaap <piretsaartok@gmail.com>
2025-10-13 18:12:02 +0300

    Uploading

--- SQL Queries/sales_analysis.sql
+++ SQL Queries/sales_analysis.sql
@@ -5,8 +5,8 @@
 SELECT * FROM SalesTable LIMIT 10;
 -- 1.2 SalesRepTable
 SELECT * FROM SalesRepTable LIMIT 10;
--- 1.3 BudgetSalesRep
-SELECT * FROM BudgetSalesRep LIMIT 10;
+-- 1.3 BudgetTable
+SELECT * FROM BudgetTable LIMIT 10;
 
 -- 2. Finding total sales by each sales year.
 SELECT EXTRACT(
@@ -18,21 +18,20 @@ SELECT EXTRACT(
         )::numeric, 0
     ) AS total_sales
 FROM SalesTable
-GROUP BY
-    sales_year
+GROUP BY sales_year
 ORDER BY sales_year asc;
 
 --3. Comparing total sales by each sales year to the total budgeted sales each year.
 WITH
     -- Creating a table for total_budget.
     B AS (
-        SELECT EXTRACT(
-                YEAR
-                FROM budget_date
-            ) AS budget_year, ROUND(SUM(budget_sum)) AS total_budget
-        FROM BudgetTable
-        GROUP BY
-            budget_year
+    SELECT EXTRACT(
+            YEAR
+            FROM DT 
+        ) AS budget_year, ROUND(SUM(Budget)) AS total_budget 
+    FROM BudgetTable 
+    GROUP BY
+        budget_year
     ),
     -- Creating a table for total_sales.
     S AS (
@@ -52,13 +51,13 @@ WITH
     -- Selecting the total budget sum and total sales sum into one table to compare the results.
 SELECT B.budget_year, B.total_budget, S.total_sales, S.total_sales - B.total_budget AS difference,
 FROM B
-    LEFT JOIN B ON B.budget_year = S.sales_year;
+    LEFT JOIN S ON B.budget_year = S.sales_year;
 
 -- Comparing the results.
 --What is the average, minimum and maximum quantity sold by product in 2025?
-select product_id, round(avg(quantity)), min(quantity), max(quantity)
-from sales_table
-where
+SELECT product_id, round(avg(quantity)), min(quantity), max(quantity)
+FROM sales_table
+WHERE
     sale_date >= '2025-01-01'
-group by
+GROUP BY
     product_id;
